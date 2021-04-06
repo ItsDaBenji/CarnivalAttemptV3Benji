b24e24046693c6306b037e7fb96844875145c6a2
Benjamin Blackmon <benmblackmon@gmail.com>
2021-04-05 19:30:19 -0500

    Messed with things, I think everything works

--- Assets/BenjimanCarnival/BMBScripts/BMBSpawnCanister.cs
+++ Assets/BenjimanCarnival/BMBScripts/BMBSpawnCanister.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class BMBSpawnCanister : MonoBehaviour
 {
 
-    [SerializeField] Transform[] spawnpoints;
+    [SerializeField] Transform posOne, posTwo, posThree, posFour, posFive, posSix;
+
+    [SerializeField] List<Transform> spawnpoints = new List<Transform>(); //serialized for debugging purposes
 
     [SerializeField] GameObject[] canisterPrefabs;
 
@@ -13,14 +15,29 @@ public class BMBSpawnCanister : MonoBehaviour
 
     private void Start()
     {
+        spawnpoints.Add(posOne);
+        spawnpoints.Add(posTwo);
+        spawnpoints.Add(posThree);
+        spawnpoints.Add(posFour);
+        spawnpoints.Add(posFive);
+        spawnpoints.Add(posSix);
+
         InvokeRepeating("SpawnCanisterTimer", 1, 10);
     }
 
     private void SpawnCanisterTimer()
     {
         int randPrefab = Random.Range(0, canisterPrefabs.Length);
-        int randPos = Random.Range(0, spawnpoints.Length);
+        int randPos = Random.Range(0, spawnpoints.Count);
         Instantiate(canisterPrefabs[randPrefab], spawnpoints[randPos].position, Quaternion.Euler(90, 0, 0));
+        StartCoroutine(AddPosBack(spawnpoints[randPos]));
+        spawnpoints.Remove(spawnpoints[randPos]);
+    }
+
+    private IEnumerator AddPosBack(Transform removedPos)
+    {
+        yield return new WaitForSeconds(10);
+        spawnpoints.Add(removedPos);
     }
 
 }
